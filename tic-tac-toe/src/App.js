5d2bb5cbc2e255fbe82d20731d2d2769f8ac15b8
Becheru888 <becheru93@gmail.com>
2020-01-29 19:39:30 +0000

    logic implemented

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,20 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import O from "./players_O_X/O.png";
+import X from "./players_O_X/X.png";
 import "./App.css";
 
 function App() {
+  const [playerOne, setPlayerOne] = useState({
+    turn: false,
+    icon: "X"
+  });
+
+  const [playerTwo, setPlayerTwo] = useState({
+    turn: true,
+    icon: "O"
+  });
+
+  const handleClick = (id, e) => {
+    if (!playerOne.turn) {
+      setPlayerOne({ ...playerOne, turn: true });
+      setPlayerTwo({ ...playerTwo, turn: false });
+    } else {
+      setPlayerOne({ ...playerOne, turn: false });
+      setPlayerTwo({ ...playerTwo, turn: true });
+    }
+    console.log(playerOne.turn, playerTwo.turn);
+    return (e.target.innerHTML = playerOne.turn
+      ? playerOne.icon
+      : playerTwo.icon);
+  };
+
   return (
     <div className="App">
       <Game>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div
+          id="1"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="2"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="3"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="4"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="5"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="6"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="7"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="8"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
+        <div
+          id="9"
+          onClick={e => {
+            handleClick(e.target.id, e);
+          }}
+        ></div>
       </Game>
     </div>
   );
@@ -35,7 +106,9 @@ const Game = styled.div`
   border-radius: 20px;
 
   div {
-    border: 0.1px solid gray;
+    text-align: center;
+    border: 0.1px solid red;
     min-width: 33.2%;
+    max-height: 33.33%;
   }
 `;
